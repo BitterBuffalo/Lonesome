826f98324e98899042f72addff8e4037c0182a99
skylarromocki <skylar.romocki@gmail.com>
2016-09-06 16:55:22 -0400

    Looking around with the mouse
    Look with mouse implemented

---
+++ Lonesome/Assets/Controller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+    public GameObject player;
+    public Camera cam;
+
+    public float speedH, speedV, yaw, pitch;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+}
