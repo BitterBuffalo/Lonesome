0440dafc739edf8723f2a2e6bb2db747eabc37bf
BitterBuffalo <kfriddle@students.kennesaw.edu>
2016-09-19 15:04:51 -0400

    Made changes to Interact and DoorITBL scripts: Added a ray that the doorknob will attempt to rotate toward. Made a door prefab for demonstration of door physics.

--- Lonesome/Assets/Scripts/DoorITBL.cs
+++ Lonesome/Assets/Scripts/DoorITBL.cs
@@ -1,24 +1,39 @@
-﻿using UnityEngine;
+﻿//Kevin Friddle
+//Created: 9/17/2016
+//Last Updated: 9/19/2016
+using UnityEngine;
 using System.Collections;
 
 public class DoorITBL : Interactable {
 
     public GameObject cam;
     public float speed;
+    public Interact actor;  //object to act upon the door
     Rigidbody rb;
+    bool rdSet; //has the ray distance been set
 
 	// Use this for initialization
 	void Start () {
         rb = gameObject.GetComponent<Rigidbody>();
+        rdSet = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if(interacting && !rdSet)
+        {
+            rdSet = true;
+            actor.AnchorDistance = Vector3.Distance(cam.transform.position, transform.position);
+        }
+        else if(!interacting && rdSet)
+        {
+            rdSet = false;
+        }
 	}
 
     public override void Interact()
     {
-        rb.AddRelativeTorque(cam.transform.forward * speed);
+        //rb.AddRelativeTorque(cam.transform.forward * speed);
+        rb.angularVelocity += (actor.Anchor.GetPoint(actor.AnchorDistance) - transform.position) * speed * Time.deltaTime; 
     }
 }
--- Lonesome/Assets/Scripts/Interact.cs
+++ Lonesome/Assets/Scripts/Interact.cs
@@ -1,6 +1,6 @@
 ﻿//Kevin Friddle
 //Created: 9/6/2016
-//Last Updated: 9/17/2016
+//Last Updated: 9/19/2016
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
@@ -15,21 +15,26 @@ public class Interact : MonoBehaviour {
     //Text promptText;
     bool canInteract;
     Interactable item;
+    Ray anchor; //ray from the camera which sets a point for the doorknob of a door to follow upon interaction
+    float anchorDistance;   //distance of the anchor point from the camera
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         //promptText = prompt.GetComponent<Text>();
         prompt.SetActive(false);
 
         canInteract = false;
         hit = new RaycastHit();
-	}
+
+        anchor = new Ray(cam.transform.position, cam.transform.forward);
+        anchorDistance = 0.0f;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	    if(canInteract)
         {
-            if(Input.GetMouseButton(0))
+            if(Input.GetMouseButton(0) && !item.Interacting)
             {
                 Debug.Log("Interacted!");
                 item.Interacting = true;
@@ -37,6 +42,11 @@ public class Interact : MonoBehaviour {
             else if(Input.GetMouseButtonUp(0))
             {
                 item.Interacting = false;
+            }
+
+            if(item.Interacting)
+            {
+                anchor.direction = cam.transform.forward;
             }
         }
 	}
@@ -62,5 +72,16 @@ public class Interact : MonoBehaviour {
             prompt.SetActive(false);
             canInteract = false;
         }
+    }
+
+    public Ray Anchor
+    {
+        get { return anchor; }
+    }
+
+    public float AnchorDistance
+    {
+        get { return anchorDistance; }
+        set { anchorDistance = value; }
     }
 }
