608cf52a49676026114b5565018673105c56d2c4
culrich2 <culrich2@students.kennesaw.edu>
2016-09-30 23:23:41 -0400

    CharacterController and ThirdFloor Layout Model Added. New Unity Store Assets Included
    I've added the CharacterController and ThirdFloor Layout model to this
    update. These changes can be seen in the FloorLayoutDemo. Also, Fowler
    sent out the link to our models, so I've included the Office assets and
    one of the monsters if you guys want to go ahead and start looking at
    them. If there are issues let me know.

---
+++ Lonesome/Assets/CharacterController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+    public float speed = 1.0f;
+
+    // Use this for initialization
+	void Start ()
+    {
+        //Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Vertical") * speed;
+        float moveVertical = Input.GetAxis("Horizontal") * speed;
+
+        transform.Translate(moveVertical, 0, moveHorizontal);
+
+        if(Input.GetKeyDown("escape"))
+        {
+            //Cursor.lockState = CursorLockMode.None;
+        }
+
+	}
+}
